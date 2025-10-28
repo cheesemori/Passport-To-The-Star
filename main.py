2c62c013ab831dfadff5905e8a2726c4b71322cc
cheesemori <2274159818@qq.com>
2025-10-28 20:44:22 +0000

    add feature: highlight button when mouse hover on it

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 import sys
 
+
 pygame.init()
 
 game_version = "v0.1"
 screen = pygame.display.set_mode((800, 640))
 pygame.display.set_caption(f'Passport To The Star {game_version}')
+
+
 
 background_image = pygame.image.load('image/background.png').convert_alpha()
 background = pygame.transform.scale(background_image, (800, 640))
@@ -16,12 +19,22 @@ start_logo = pygame.transform.scale(start_logo_image, (600,600))
 title_logo_image = pygame.image.load('image/title_logo.png').convert_alpha()
 title_logo = pygame.transform.scale(title_logo_image, (512,64))
 
-start_button_image = pygame.image.load('image/start.png').convert_alpha()
-start_button = pygame.transform.scale(start_button_image, (150,80))
+start_button_normal_image = pygame.image.load('image/start.png').convert_alpha()
+start_button_normal = pygame.transform.scale(start_button_normal_image, (150, 80))
 credit_button_image = pygame.image.load('image/credit.png').convert_alpha()
 credit_button = pygame.transform.scale(credit_button_image, (150,80))
-quit_button_image = pygame.image.load('image/quit.png').convert_alpha()
-quit_button = pygame.transform.scale(quit_button_image, (150,80))
+quit_button_normal_image = pygame.image.load('image/quit.png').convert_alpha()
+quit_button_normal = pygame.transform.scale(quit_button_normal_image, (150, 80))
+
+start_button_hover_image = pygame.image.load('image/start_hover.png').convert_alpha()
+start_button_hover = pygame.transform.scale(start_button_hover_image, (150,80))
+quit_button_hover_image = pygame.image.load('image/quit_hover.png').convert_alpha()
+quit_button_hover = pygame.transform.scale(quit_button_hover_image, (150,80))
+
+start_button_pressed_image = pygame.image.load('image/start_pressed.png').convert_alpha()
+start_button_pressed = pygame.transform.scale(start_button_pressed_image, (150,80))
+quit_button_pressed_image = pygame.image.load('image/quit_pressed.png').convert_alpha()
+quit_button_pressed = pygame.transform.scale(quit_button_pressed_image, (150,80))
 
 splash_logo_alpha = 0
 background_alpha = 0
@@ -31,11 +44,23 @@ credit_button_alpha = 0
 quit_button_alpha = 0
 
 splash_state = "fade_in"
-
+menu_state = ""
 game_state = "splash"
-
 title_state = "off"
 
+start_button_range = ((105,466),(235,534))
+quit_button_range = ((105,556),(228,624))
+
+start_button = [start_button_normal,start_button_hover,start_button_pressed]
+quit_button = [quit_button_normal,quit_button_hover,quit_button_pressed]
+start_button_index = 0
+quit_button_index = 0
+
+def mouse_in_start_button():
+    return start_button_range[0][0] <= mouse_x <= start_button_range[1][0] and start_button_range[0][1] <= mouse_y <= start_button_range[1][1]
+
+def mouse_in_quit_button():
+    return quit_button_range[0][0] <= mouse_x <= quit_button_range[1][0] and quit_button_range[0][1] <= mouse_y <= quit_button_range[1][1]
 running = True
 while running:
     for event in pygame.event.get():
@@ -43,7 +68,9 @@ while running:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            print(f"鼠标点击坐标: {event.pos}")
+            print(f"mouse position: {event.pos}")
+
+    mouse_x, mouse_y = pygame.mouse.get_pos()
 
     if game_state == "splash":
 
@@ -65,29 +92,42 @@ while running:
             splash_logo_alpha -= 3
             if splash_logo_alpha <= 0:
                 game_state = "main_menu"
-
+                menu_state = "fade_in"
 
     elif game_state == "main_menu":
-        if background_alpha < 255:
-            background_alpha += 5
-        elif background_alpha >= 255:
-            title_state = "on"
-            if title_logo_alpha < 255:
-                title_logo_alpha += 5
-                start_button_alpha += 5
-                credit_button_alpha += 5
-                quit_button_alpha += 5
+        if menu_state == "fade_in":
+            if background_alpha < 255:
+                background_alpha += 5
+            elif background_alpha >= 255:
+                title_state = "on"
+                if title_logo_alpha < 255:
+                    title_logo_alpha += 5
+                    start_button_alpha += 5
+                    credit_button_alpha += 5
+                    quit_button_alpha += 5
+
+        if mouse_in_start_button():
+            start_button_index = 1
+        elif mouse_in_quit_button():
+            quit_button_index = 1
+        else:
+            start_button_index = 0
+            quit_button_index = 0
+
+    background.set_alpha(background_alpha)
+    title_logo.set_alpha(title_logo_alpha)
+    start_button_normal.set_alpha(start_button_alpha)
+    # credit_button.set_alpha(credit_button_alpha)
+    quit_button_normal.set_alpha(quit_button_alpha)
+
+    screen.blit(background, (0, 0))
+    screen.blit(title_logo, title_logo.get_rect(center=(230, 70)))
+    screen.blit(start_button[start_button_index], start_button_normal.get_rect(center=(170, 500)))
+    screen.blit(quit_button[quit_button_index], quit_button_normal.get_rect(center=(170, 590)))
 
 
-        background.set_alpha(background_alpha)
-        screen.blit(background, (0, 0))
-        title_logo.set_alpha(title_logo_alpha)
-        screen.blit(title_logo, title_logo.get_rect(center=(230,70)))
-        start_button.set_alpha(start_button_alpha)
-        credit_button.set_alpha(credit_button_alpha)
-        quit_button.set_alpha(quit_button_alpha)
-        screen.blit(start_button, start_button.get_rect(center=(170,500)))
-        screen.blit(quit_button,quit_button.get_rect(center=(170,590)))
+
+
 
 
     pygame.display.update()
